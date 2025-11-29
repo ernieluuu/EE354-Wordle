acc483eff87d3c23f4bffd98013654f075c65f4e
Ernest Lu <ernestlu@mac.lan>
2025-11-29 00:21:50 -0700

    updated word_format

--- word_format.v
+++ word_format.v
@@ -26,8 +26,8 @@ module word_format(
 	
 	
 	output q_I, output q_Let, output q_Pos,
-	output pos;
-	output [4:0] word_array [0:4], // A-Z = 26 letters = 5 bits
+	output reg [2:0] pos,
+	output reg [4:0] word_array [0:4] // A-Z = 26 letters = 5 bits
 	);
 	
 	reg [2:0] state;
@@ -35,15 +35,21 @@ module word_format(
 	localparam
 	I = 3'b001, LetterChange = 3'b010, PosChange = 3'b100, UNK = 3'bXXX;
 
-	// wires and registers
-	wire [2:0] pos; // 5 positions for letters
+	// wires and registers - NOTE: commented out bc its alr declared in the output declaration
+	//reg [2:0] pos; // 5 positions for letters
+	//reg [4:0] word_array [0:4]; // 5 index array of 5 bit letters
 	
 	always @ (posedge Clk, posedge RESET)
 	begin
 		if(RESET)
 		begin
-			pos <= 3'bXXX;
+			pos <= 0;
 			state <= I;
+			word_array[0] <= 5'd0;  // 'A'
+			word_array[1] <= 5'd0;  // 'A'
+			word_array[2] <= 5'd0;  // 'A'
+			word_array[3] <= 5'd0;  // 'A'
+			word_array[4] <= 5'd0;  // 'A'
 		end
 		else
 			case(state)
@@ -60,15 +66,27 @@ module word_format(
 					begin
 						if(UP)
 						begin
+							if (word_array[pos] >= 25)
+								word_array[pos] <= 0;
+							else
+								word_array[pos] <= word_array[pos] + 1;
+							/*
 							word_array[pos] <= word_array[pos] + 1;
-							if(word_array[pos] == 25 || 26)
+							if(word_array[pos] == 25 || word_array[pos] == 26)
 								word_array[pos] <= 0; // wrap around back to 0
+							*/
 						end
 						else if(DOWN)
 						begin
+							if (word_array[pos] == 0)
+								word_array[pos] <= 25;
+							else
+								word_array[pos] <= word_array[pos] - 1;
+							/*
 							word_array[pos] <= word_array[pos] - 1;
 							if(word_array[pos] == 0)
 								word_array[pos] <= 25; // wrap around back to 25
+							*/
 						end
 						else if (LEFT || RIGHT)
 							state <= PosChange;
@@ -79,6 +97,7 @@ module word_format(
 				PosChange:
 				if(SCEN)
 					begin
+						/*
 						if(RIGHT)
 						begin
 							pos <= pos + 1;
@@ -90,6 +109,21 @@ module word_format(
 							pos <= pos - 1;
 							if(pos == 0)
 								pos <= 5;
+						end
+						*/
+						if(RIGHT)
+						begin
+							if(pos == 4)
+								pos <= 0;
+							else
+								pos <= pos + 1;
+						end
+						else if(LEFT)
+						begin
+							if(pos == 0)
+								pos <= 4;
+							else
+								pos <= pos - 1;
 						end
 						else if(UP || DOWN)
 							state <= LetterChange;
