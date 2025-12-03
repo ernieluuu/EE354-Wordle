a13e94d8652893e6484074ab644fb5c9d340d68c
jiba1io <jibalio@usc.edu>
2025-12-03 12:46:29 -0800

    debounce ported in from class

---
+++ ee201_debounce_DPB_SCEN_CCEN_MCEN.v
@@ -0,0 +1,164 @@
+//////////////////////////////////////////////////////////////////////////////////
+// 
+// Author: Gandhi Puvvada
+// Create Date:   08/21/09,  Minor Revision: 2/10/2012, 9/24/2014, 2/18/2020
+// File Name:	ee201_debouncer.v 
+// Description: Debouncer module
+// Additional Comments: Translated from VHDL by Matthew Christian and Gandhi Puvvada 2/21/2010
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+module ee201_debouncer(CLK, RESET, PB, DPB, SCEN, MCEN, CCEN);
+
+//inputs
+input	CLK, RESET;
+input PB;
+
+//outputs
+output DPB;
+output SCEN, MCEN, CCEN;
+
+//parameters
+parameter N_dc = 5;
+
+//local variables
+// (* fsm_encoding = "user" *) worked with the earlier Xilinx systhesis tool called XST (part of the ISE suite).
+// The current Xilinx Vivado does not provide user as an option for FSM encoding.
+// Here, we however show how to specify this though it is going to be ignored by the current Vivado tool.
+
+// The next line is an attribute specification allowed by Verilog 2001. 
+// The "* fsm_encoding = "user" *"  tells XST synthesis tool that the user chosen 
+// state codes are retained. This is essential for our carefully chosen output-coding to 
+// prevail and make the OFL "null" there by ensuring that the outputs will not have 
+// glitches. Frankly, it is not needed as we are *not* using these SCEN and MCEN pulses 
+// as clock pulses. We are rather using them as enable pulses enabling the clock 
+// (or more appropriately enabling the data).
+
+(* fsm_encoding = "user" *)
+reg [5:0] state;
+// other items not controlled by the special attribute
+reg [N_dc-1:0] debounce_count;
+reg [3:0] MCEN_count;
+
+
+//concurrent signal assignment statements
+// The following is possible because of the output coding used by us.
+assign {DPB, SCEN, MCEN, CCEN} = state[5:2];
+
+//constants used for state naming // the don't cares are replaced here with zeros
+localparam
+ INI        = 6'b000000,
+ WQ         = 6'b000001,
+ SCEN_st    = 6'b111100,
+ WH         = 6'b100000,
+ MCEN_st    = 6'b101100,
+ CCEN_st    = 6'b100100,
+ MCEN_cont  = 6'b101101,
+ CCR        = 6'b100001,
+ WFCR       = 6'b100010;
+		      
+//logic
+always @ (posedge CLK, posedge RESET)
+	begin : State_Machine
+		
+		if (RESET)
+		   begin
+		      state <= INI;
+		      debounce_count <= 'bx;
+		      MCEN_count <= 4'bx;
+		   end
+		else 
+		   begin
+			   case (state)
+				   INI: begin					
+					     debounce_count <= 0;
+					     MCEN_count <= 0;  
+					     if (PB)
+						   begin
+							   state <= WQ;
+						   end
+			            end
+			       WQ: begin
+					   debounce_count <= debounce_count + 1;
+			           if (!PB)
+			             begin
+			                state <= INI;
+			             end
+				       else if (debounce_count[N_dc-2]) // for N_dc of 25, it is debounce_count[23], i.e T = 0.084 sec for f = 100MHz
+				         begin
+				             state <= SCEN_st;
+				         end
+				      end
+				   SCEN_st: begin
+					  debounce_count <= 0;
+				      MCEN_count <= MCEN_count + 1;
+				      state <= WH;
+				      end
+				   
+				   WH: begin
+					  debounce_count <= debounce_count + 1;
+				      if (!PB)
+				         begin
+								state <= CCR;
+							end	
+				      else if (debounce_count[N_dc-1]) // for N_dc of 25, it is debounce_count[24], i.e T = 0.168 sec for f = 100MHz
+				         begin
+								state <= MCEN_st;
+							end
+				      end
+				      
+				   MCEN_st: begin
+					  debounce_count <= 0;
+				      MCEN_count <= MCEN_count + 1;
+				      state <= CCEN_st;
+				      end
+				   
+				   CCEN_st: begin
+					  debounce_count <= debounce_count + 1;
+				      if (!PB)
+				        begin
+							state <= CCR;
+						end
+				      else if (debounce_count[N_dc-2]) // for N_dc of 25, it is debounce_count[23], i.e T = 0.084 sec for f = 100MHz
+				        begin
+				            if (MCEN_count == 4'b1000)
+				                begin
+									state <= MCEN_cont;
+								end
+				            else
+				                begin
+									state <= MCEN_st;
+								end
+				        end
+				      end
+				      
+				   MCEN_cont: begin // remain here until the PB is released
+					  if (!PB)
+				         begin
+								state <= CCR;
+						 end
+				      end
+				   
+				   CCR: begin
+					  debounce_count <= 0;
+				      MCEN_count <= 0;
+				      state <= WFCR;
+				      end
+				   
+				   WFCR: begin
+					  debounce_count <= debounce_count + 1;
+				      if (PB)
+				         begin
+								state <= WH;
+							end
+				      else if (debounce_count[N_dc-2])// for N_dc of 25, it is debounce_count[23], i.e T = 0.084 sec for f = 100MHz
+				         begin
+								state <= INI;
+							end
+				      end
+				endcase		    
+	      end
+	end // State_Machine
+
+	
+endmodule // ee201_debouncer
